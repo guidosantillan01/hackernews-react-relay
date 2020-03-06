30bf63e6603fa0c5023efeb3d2c697f9ca505c4d
Guido Santillan <guidosantillan01@gmail.com>
2020-03-06 11:19:19 +0100

    Refactor LinkListPage

--- src/components/LinkListPage.js
+++ src/components/LinkListPage.js
@@ -13,20 +13,22 @@ const LinkListPageQuery = graphql`
   }
 `;
 
+const renderComponent = ({ error, props }) => {
+  if (error) {
+    return <div>{error.message}</div>;
+  } else if (props) {
+    return <LinkList viewer={props.viewer} />;
+  }
+  return <div>Loading...</div>;
+};
+
 class LinkListPage extends Component {
   render() {
     return (
       <QueryRenderer
         environment={environment}
         query={LinkListPageQuery}
-        render={({ error, props }) => {
-          if (error) {
-            return <div>{error.message}</div>;
-          } else if (props) {
-            return <LinkList viewer={props.viewer} />;
-          }
-          return <div>Loading</div>;
-        }}
+        render={renderComponent}
       />
     );
   }
